f8ac8a8812b5636ba91fe3e5c3a4b5e9b4a951a0
vitorcosta26 <60762180+vitorcosta26@users.noreply.github.com>
2023-05-29 01:42:00 -0300

    update

--- lib/src/view/widget/carousel_slider.dart
+++ lib/src/view/widget/carousel_slider.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 class CarouselSlider extends StatefulWidget {
   const CarouselSlider({
